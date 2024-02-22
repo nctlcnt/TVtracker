de0f3bd986afea8dea82d481abaeff02bb96225b
chachaya <cece.ch3@gmail.com>
2024-02-22 20:59:02 +0800

    change env url

--- src/apis/mongodbAPI.ts
+++ src/apis/mongodbAPI.ts
@@ -1,6 +1,6 @@
 // For mongoDB APIs
 // users
-export const mongoBaseUrl: string = `${process.env.BACKEND_URL}:5050`
+export const mongoBaseUrl: string = process.env.BACKEND_URL
 export const getUsers: string = mongoBaseUrl + '/users'
 export const getUserInfo: string = mongoBaseUrl + '/users/{userId}'
 
